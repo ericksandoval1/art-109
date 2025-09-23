968364f041622bf47741051b3b04d53f172da2a3
ericksandoval1 <ericksandoval1414@gmail.com>
2025-09-23 12:31:20 -0700

    05 dom + javascript interaction

---
+++ tutorials/dom/js/main.js
@@ -0,0 +1,25 @@
+
+document.querySelector("#image0").addEventListener("click", function() { 
+    console.log("here");
+    document.querySelector("#image1").style.visibility = "visible";
+})
+
+document.querySelector("#image1").addEventListener("click", function() { 
+    document.querySelector("#image2").style.visibility = "visible";
+})
+
+document.querySelector("#image2").addEventListener("click", function() { 
+    document.querySelector("#image3").style.visibility = "visible";
+})
+
+document.querySelector("#image3").addEventListener("click", function() { 
+    document.querySelector("#image4").style.visibility = "visible";
+})
+
+document.querySelector("#image4").addEventListener("click", function() { 
+    document.querySelector("#image5").style.visibility = "visible";
+})
+
+document.querySelector("#image5").addEventListener("click", function() { 
+})
+
